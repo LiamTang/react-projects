bd4053d3a404804b0f2a1e68622f119043478e1a
liam tang <tangwangjun123@gmail.com>
2020-11-13 21:20:49 +1100

    complete practice 12

---
+++ projects/12-sidebar-modal/setup/src/App.js
@@ -0,0 +1,17 @@
+/** @format */
+
+import React, { Fragment } from "react";
+import Modal from "./Modal";
+import Sidebar from "./Sidebar";
+import Home from "./Home";
+function App() {
+	return (
+		<Fragment>
+			<Home />
+			<Modal />
+			<Sidebar />
+		</Fragment>
+	);
+}
+
+export default App;
