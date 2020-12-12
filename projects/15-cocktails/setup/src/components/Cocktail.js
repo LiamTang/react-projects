f71984f450f736cefce8ece78dafc2491c81ffb2
Liam Tang <tangwangjun123@gmail.com>
2020-12-12 22:28:31 +1100

    complete part of practice 15

--- projects/15-cocktails/setup/src/components/Cocktail.js
+++ projects/15-cocktails/setup/src/components/Cocktail.js
@@ -1,12 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+/** @format */
 
-const Cocktail = () => {
-  return (
-    <div>
-      <h2>cocktail component</h2>
-    </div>
-  )
-}
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default Cocktail
+const Cocktail = ({ id, image, name, info, glass }) => {
+	return (
+		<article className="cocktail">
+			<div className="img-container">
+				<img src={image} alt={name} />
+			</div>
+			<div className="cocktail-footer">
+				<h3>{name}</h3>
+				<h4>{glass}</h4>
+				<p>{info}</p>
+				<Link
+					to={`/cocktail/${id}`}
+					className="btn btn-primary btn-details"
+				>
+					Details
+				</Link>
+			</div>
+		</article>
+	);
+};
+
+export default Cocktail;
